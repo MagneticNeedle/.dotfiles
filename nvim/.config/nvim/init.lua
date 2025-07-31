dbea492c61cb6147bd0a1ae96ad93b3033fbfacd
MagneticNeedle <solankivibhakar82@gmail.com>
2025-07-31 08:57:08 +0530

    feat: add nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,20 @@
+require("config.lazy")
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.diagnostic.config({ virtual_text = true })
+vim.cmd.colorscheme("gruber-darker")
+vim.lsp.enable("basedpyright")
+vim.lsp.enable("lua_ls")
